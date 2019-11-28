ced142568752f9ae5947cd31bf84d3926563de2b
dej <dej@vologda.ru>
2019-11-28 08:00:02 +0300

    Add some changes in def()

--- competition.py
+++ competition.py
@@ -5,48 +5,14 @@ import re
 import argparse
 
 
-def get_username_and_id(comment):
-    user_id = comment['user_id']
-    username = comment['user']['username']
-    return user_id, username
+def is_users_followed(comment, followed_users):
+    followed_user_id = str(comment['user_id'])
+    return followed_user_id in followed_users
 
 
-def get_users_who_tag_friends(bot, comments):
-    marked_friends = []
-    for comment in comments:
-        text = comment['text']
-        friends = get_usernames_from_comment(text)
-        for friend in friends:
-            if is_user_exists(bot, friend):
-                marked_friends.append(get_username_and_id(comment))
-                break
-
-    return marked_friends
-
-
-def get_users_liked_post(comments):
-    liked_comments = []
-    for comment in comments:
-        if 'comment_like_count' in comment:
-            liked = comment['comment_like_count']
-            if liked > 0:
-                liked_comments.append(get_username_and_id(comment))
-
-    return liked_comments
-
-
-def get_followed_users(bot, comments, media_id):
-    media_owner_id = bot.get_media_owner(media=media_id)
-    media_owner = bot.get_username_from_user_id(media_owner_id)
-    author_followers = bot.get_user_followers(media_owner)
-
-    followed_users = []
-    for comment in comments:
-        user_id = str(comment['user_id'])
-        if user_id in author_followers:
-            followed_users.append(get_username_and_id(comment))
-
-    return followed_users
+def is_user_liked(comment):
+    liked = 'comment_like_count'
+    return liked in comment and comment[liked] > 0
 
 
 def get_usernames_from_comment(comment):
@@ -57,25 +23,17 @@ def get_usernames_from_comment(comment):
 
 
 def is_user_exists(bot, username):
-    result = bot.get_user_id_from_username(username)
-    if result:
-        return True
+    return bot.get_user_id_from_username(username) is not None
+
+
+def is_user_tag_friends(bot, comment):
+    text_comment = comment['text']
+    tagged_friends = get_usernames_from_comment(text_comment)
+
+    for friend in tagged_friends:
+        if is_user_exists(bot, friend):
+            return True
     return False
-
-
-def get_action_winners(bot, url):
-    media_id = bot.get_media_id_from_link(url)
-    comments = bot.get_media_comments_all(media_id=media_id)
-
-    users_liked_post = set(get_users_liked_post(comments))
-    users_who_tag_friends = set(get_users_who_tag_friends(bot, comments))
-    followed_users = set(get_followed_users(bot, comments,  media_id))
-
-    winners = users_liked_post.intersection(followed_users). \
-        intersection(users_who_tag_friends)
-    winners = [winner[1] for winner in winners]
-
-    return winners
 
 
 def main():
@@ -96,13 +54,34 @@ def main():
         os.makedirs(instagram_dir)
 
     bot = Bot(base_path=instagram_dir)
-    bot.login(username=login, password=password)
+    bot.login(username=login, password=password, force=True)
 
-    action_winners = get_action_winners(bot=bot, url=instagram_url)
+    media_id = bot.get_media_id_from_link(instagram_url)
+    media_owner_id = bot.get_media_owner(media=media_id)
+    media_owner = bot.get_username_from_user_id(media_owner_id)
+    author_followers = bot.get_user_followers(media_owner)
+    comments = bot.get_media_comments_all(media_id=media_id)
+
+    print('end of fetching data')
+
+    finalists = []
+
+    for comment in comments:
+        if not is_user_liked(comment):
+            continue
+
+        if not is_users_followed(comment, author_followers):
+            continue
+
+        if not is_user_tag_friends(bot, comment):
+            continue
+
+        username = comment['user']['username']
+        finalists.append(username)
 
     bot.logout()
-
-    print(action_winners)
+    winners = set(finalists)
+    print(winners)
 
 
 if __name__ == '__main__':
